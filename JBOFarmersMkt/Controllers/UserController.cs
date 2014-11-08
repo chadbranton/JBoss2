7caf4d77461096d49fd34df84b2b0bfa6597187b
chadbranton <branton@goldmail.etsu.edu>
2014-11-08 15:50:51 -0500

    Add Roles
    Added Roles.  Changed User Profiles to reflect customer data.  Customer
    model can now be removed.  protected all admin controllers with Roles.

---
+++ JBOFarmersMkt/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using JBOFarmersMkt.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JBOFarmersMkt.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        
+        JBOContext context = new JBOContext();
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Index()
+        {
+            var users = from user in context.UserProfiles select user;
+
+            return View(users);
+        }
+
+    }
+}
