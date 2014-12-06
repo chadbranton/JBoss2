6a4f447e9d762d20dafa6beabe26cfa0ca85103b
Ryan Robeson <ryan.robeson@gmail.com>
2014-12-06 02:51:11 -0500

    Get roles working on my machine.
    Add support for seeding roles and users in the database.
    Commit the database with the admin user included.

--- JBOFarmersMkt/Migrations/Configuration.cs
+++ JBOFarmersMkt/Migrations/Configuration.cs
@@ -1,9 +1,14 @@
 namespace JBOFarmersMkt.Migrations
 {
     using System;
+    using JBOFarmersMkt.Context;
+    using JBOFarmersMkt.Models;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web.Security;
+    using System.Web.Mvc;
+    using WebMatrix.WebData;
 
     internal sealed class Configuration : DbMigrationsConfiguration<JBOFarmersMkt.Context.JBOContext>
     {
@@ -15,7 +20,27 @@ namespace JBOFarmersMkt.Migrations
         protected override void Seed(JBOFarmersMkt.Context.JBOContext context)
         {
             //  This method will be called after migrating to the latest version.
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: false);
+            }
 
+            //WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: false);
+
+            if (!Roles.RoleExists("Administrator"))
+            {
+                Roles.CreateRole("Administrator");
+            }
+
+            if (!WebSecurity.UserExists("admin"))
+            {
+                WebSecurity.CreateUserAndAccount("admin", "password");
+            }
+
+            if (!Roles.GetRolesForUser("admin").Contains("Administrator"))
+            {
+                Roles.AddUserToRole("admin", "Administrator");
+            }
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
@@ -26,6 +51,8 @@ namespace JBOFarmersMkt.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            base.Seed(context);
         }
     }
 }
