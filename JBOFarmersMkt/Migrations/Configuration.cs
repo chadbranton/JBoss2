3a05b02c38a54179b19d1513749ce9163e4f9ff9
Ryan Robeson <ryan.robeson@gmail.com>
2014-12-07 00:12:59 -0500

    Add some more development seed data

--- JBOFarmersMkt/Migrations/Configuration.cs
+++ JBOFarmersMkt/Migrations/Configuration.cs
@@ -9,6 +9,7 @@ namespace JBOFarmersMkt.Migrations
     using System.Web.Security;
     using System.Web.Mvc;
     using WebMatrix.WebData;
+    using System.Collections.Generic;
 
     internal sealed class Configuration : DbMigrationsConfiguration<JBOFarmersMkt.Context.JBOContext>
     {
@@ -25,22 +26,92 @@ namespace JBOFarmersMkt.Migrations
                 WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: false);
             }
 
-            //WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: false);
-
-            if (!Roles.RoleExists("Administrator"))
+            // Initialize roles
+            List<string> roles = new List<string> { "Administrator", "Developer", "Supplier", "Member" };
+            foreach (var role in roles)
             {
-                Roles.CreateRole("Administrator");
+                if (!Roles.RoleExists(role))
+                {
+                    Roles.CreateRole(role);
+                }
             }
 
-            if (!WebSecurity.UserExists("admin"))
+            // Initialize Users. This should be updated in production.
+            // Passwords should definitely not be "password".
+            // Consider: http://www.asp.net/identity/overview/features-api/best-practices-for-deploying-passwords-and-other-sensitive-data-to-aspnet-and-azure
+            var users = new Dictionary<string, List<string>>() 
             {
-                WebSecurity.CreateUserAndAccount("admin", "password");
+                {"admin", new List<string>{"Administrator"}},
+                {"ryan", new List<string>{"Administrator", "Developer"}},
+                {"chad", new List<string>{"Supplier"}},
+                {"joe", new List<string>{"Supplier"}},
+                {"tom", new List<string>{"Member"}}
+            };
+
+            foreach (var user in users)
+            {
+                if (!WebSecurity.UserExists(user.Key))
+                {
+                    WebSecurity.CreateUserAndAccount(user.Key, "password");
+                }
+
+                foreach (var role in user.Value)
+                {
+                    if (!Roles.GetRolesForUser(user.Key).Contains(role))
+                    {
+                        Roles.AddUserToRole(user.Key, role);
+                    }
+                }
             }
 
-            if (!Roles.GetRolesForUser("admin").Contains("Administrator"))
+            context.SaveChanges();
+
+            // Create some suppliers
+            var joe = context.UserProfiles.First(u => u.UserName == "joe");
+            var chad = context.UserProfiles.First(u => u.UserName == "chad");
+
+            Supplier chadsVegetables = new Supplier
+                {
+                    name = "Chad's Vegetables",
+                    address = "1234 abc lane",
+                    city = "Jonesborough",
+                    state = "TN",
+                    email = "chad@example.com",
+                    phone = "4235551234"
+                };
+
+            Supplier joesApples = new Supplier
+                    {
+                        name = "Joe's Apples",
+                        address = "4567 Caramel Valley Road",
+                        city = "Morristown",
+                        state = "TN",
+                        email = "joesapples@example.com",
+                        phone = "4235551235"
+                    };
+
+            context.Suppliers.AddOrUpdate(s => s.name, chadsVegetables, joesApples);
+
+            context.SaveChanges();
+
+            // refresh
+            chadsVegetables = context.Suppliers.Find(chadsVegetables.supplierID);
+            joesApples = context.Suppliers.Find(joesApples.supplierID);
+
+            // Assign users to suppliers
+            if (!chadsVegetables.users.Contains(chad))
             {
-                Roles.AddUserToRole("admin", "Administrator");
+                chadsVegetables.users.Add(chad);
             }
+
+            if (!joesApples.users.Contains(joe))
+            {
+                joesApples.users.Add(joe);
+            }
+
+            // Save changes
+            context.SaveChanges();
+
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
